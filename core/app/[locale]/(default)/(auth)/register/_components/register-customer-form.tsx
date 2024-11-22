304fb29e029bcf112234b9ed72e7fda030ee3854
Kathiravan Balu <kathir@arizon.digital>
2024-11-22 13:07:37 +0530

    Fixed the registration page issue

--- core/app/[locale]/(default)/(auth)/register/_components/register-customer-form.tsx
+++ core/app/[locale]/(default)/(auth)/register/_components/register-customer-form.tsx
@@ -1,10 +1,10 @@
 'use client';
- 
+
 import { useTranslations } from 'next-intl';
 import { ChangeEvent, MouseEvent, useRef, useState } from 'react';
 import { useFormStatus } from 'react-dom';
 import ReCaptcha from 'react-google-recaptcha';
- 
+
 import { useAccountStatusContext } from '~/app/[locale]/(default)/account/(tabs)/_components/account-status-provider';
 import { ExistingResultType } from '~/client/util';
 import {
@@ -34,23 +34,22 @@ import {
 import { Button } from '~/components/ui/button';
 import { Field, Form, FormSubmit } from '~/components/ui/form';
 import { Message } from '~/components/ui/message';
- 
+
 import { login } from '../_actions/login';
 import { registerCustomer } from '../_actions/register-customer';
 import { getRegisterCustomerQuery } from '../page-data';
-import { log } from 'console';
- 
+
 interface FormStatus {
   status: 'success' | 'error';
   message: string;
 }
- 
+
 type CustomerFields = ExistingResultType<typeof getRegisterCustomerQuery>['customerFields'];
 type AddressFields = ExistingResultType<typeof getRegisterCustomerQuery>['addressFields'];
 type Countries = ExistingResultType<typeof getRegisterCustomerQuery>['countries'];
 type CountryCode = Countries[number]['code'];
 type CountryStates = Countries[number]['statesOrProvinces'];
- 
+
 interface RegisterCustomerProps {
   addressFields: AddressFields;
   countries: Countries;
@@ -65,17 +64,17 @@ interface RegisterCustomerProps {
     siteKey: string;
   };
 }
- 
+
 interface SumbitMessages {
   messages: {
     submit: string;
     submitting: string;
   };
 }
- 
+
 const SubmitButton = ({ messages }: SumbitMessages) => {
   const { pending } = useFormStatus();
- 
+
   return (
     <Button
       className="relative mt-8 w-fit items-center px-8 py-2"
@@ -87,7 +86,7 @@ const SubmitButton = ({ messages }: SumbitMessages) => {
     </Button>
   );
 };
- 
+
 export const RegisterCustomerForm = ({
   addressFields,
   countries,
@@ -97,7 +96,7 @@ export const RegisterCustomerForm = ({
 }: RegisterCustomerProps) => {
   const form = useRef<HTMLFormElement>(null);
   const [formStatus, setFormStatus] = useState<FormStatus | null>(null);
- 
+
   const [textInputValid, setTextInputValid] = useState<Record<string, boolean>>({});
   const [passwordValid, setPassswordValid] = useState<Record<string, boolean>>({
     [FieldNameToFieldId.password]: true,
@@ -110,21 +109,21 @@ export const RegisterCustomerForm = ({
   const [picklistValid, setPicklistValid] = useState<Record<string, boolean>>({});
   const [checkboxesValid, setCheckboxesValid] = useState<Record<string, boolean>>({});
   const [multiTextValid, setMultiTextValid] = useState<Record<string, boolean>>({});
- 
+
   const reCaptchaRef = useRef<ReCaptcha>(null);
   const [reCaptchaToken, setReCaptchaToken] = useState('');
   const [isReCaptchaValid, setReCaptchaValid] = useState(true);
- 
+
   const { setAccountState } = useAccountStatusContext();
- 
+
   const t = useTranslations('Register.Form');
- 
+
   const handleTextInputValidation = (e: ChangeEvent<HTMLInputElement>) => {
     const fieldId = Number(e.target.id.split('-')[1]);
- 
+
     const validityState = e.target.validity;
     const validationStatus = validityState.valueMissing || validityState.typeMismatch;
- 
+
     setTextInputValid({ ...textInputValid, [fieldId]: !validationStatus });
   };
   const handleNumbersInputValidation = createNumbersInputValidationHandler(
@@ -138,38 +137,38 @@ export const RegisterCustomerForm = ({
   const handleDatesValidation = createDatesValidationHandler(setDatesValid, datesValid);
   const handlePasswordValidation = (e: ChangeEvent<HTMLInputElement>) => {
     const fieldId = e.target.id.split('-')[1] ?? '';
- 
+
     switch (FieldNameToFieldId[Number(fieldId)]) {
       case 'password': {
         setPassswordValid((prevState) => ({
           ...prevState,
           [fieldId]: !e.target.validity.valueMissing,
         }));
- 
+
         return;
       }
- 
+
       case 'confirmPassword': {
         const confirmPassword = e.target.value;
         const field = customerFields.find(
           ({ entityId }) => entityId === Number(FieldNameToFieldId.password),
         );
- 
+
         if (!isAddressOrAccountFormField(field)) {
           return;
         }
- 
+
         const passwordFieldName = createFieldName(field, 'customer');
         const password = new FormData(e.target.form ?? undefined).get(passwordFieldName);
- 
+
         setPassswordValid((prevState) => ({
           ...prevState,
           [fieldId]: password === confirmPassword && !e.target.validity.valueMissing,
         }));
- 
+
         return;
       }
- 
+
       default: {
         setPassswordValid((prevState) => ({
           ...prevState,
@@ -178,10 +177,10 @@ export const RegisterCustomerForm = ({
       }
     }
   };
- 
+
   const handleCountryChange = (value: string) => {
     const states = countries.find(({ code }) => code === value)?.statesOrProvinces;
- 
+
     setCountryStates(states ?? []);
   };
   const handleRadioButtonsChange = createRadioButtonsValidationHandler(
@@ -204,59 +203,59 @@ export const RegisterCustomerForm = ({
       validateCheckboxFields(form.current);
     }
   };
- 
+
   const onReCaptchaChange = (token: string | null) => {
     if (!token) {
       setReCaptchaValid(false);
- 
+
       return;
     }
- 
+
     setReCaptchaToken(token);
     setReCaptchaValid(true);
   };
- 
+
   const onSubmit = async (formData: FormData) => {
     if (formData.get('customer-password') !== formData.get('customer-confirmPassword')) {
       setFormStatus({
         status: 'error',
         message: t('confirmPassword'),
       });
- 
+
       window.scrollTo({
         top: 0,
         behavior: 'smooth',
       });
- 
+
       return;
     }
- 
+
     if (reCaptchaSettings?.isEnabledOnStorefront && !reCaptchaToken) {
       setReCaptchaValid(false);
- 
+
       return;
     }
- 
+
     setReCaptchaValid(true);
- 
+
     const submit = await registerCustomer({ formData, reCaptchaToken });
- 
+
     if (submit.status === 'success') {
       setAccountState({ status: 'success' });
- 
+
       await login(formData);
     }
- 
+
     if (submit.status === 'error') {
       setFormStatus({ status: 'error', message: submit.error ?? '' });
     }
- 
+
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
   };
- 
+
   return (
     <>
       {formStatus && (
@@ -266,7 +265,7 @@ export const RegisterCustomerForm = ({
       )}
       <Form action={onSubmit} onClick={preSubmitFieldsValidation} ref={form}>
         <div className="mb-4 grid grid-cols-1 gap-y-3 sm:grid-cols-2 sm:gap-x-6">
- 
+
           {customerFields
             .filter((field) => !CUSTOMER_FIELDS_TO_EXCLUDE.includes(field.entityId))
             .map((field) => {
@@ -285,7 +284,7 @@ export const RegisterCustomerForm = ({
                       />
                     </FieldWrapper>
                   );
- 
+
                 case 'MultilineTextFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -298,7 +297,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'NumberFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -311,7 +310,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'DateFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -325,7 +324,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'RadioButtonsFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -338,7 +337,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'PicklistFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -352,7 +351,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'CheckboxesFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -366,7 +365,7 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 case 'PasswordFormField': {
                   return (
                     <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -379,15 +378,15 @@ export const RegisterCustomerForm = ({
                     </FieldWrapper>
                   );
                 }
- 
+
                 default:
                   return null;
               }
             })}
-            {addressFields.map((field) => {
+          {addressFields.map((field) => {
             const fieldId = field.entityId;
             const fieldName = createFieldName(field, 'address');
- 
+
             switch (field.__typename) {
               case 'TextFormField': {
                 return (
@@ -401,7 +400,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'MultilineTextFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -414,7 +413,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'NumberFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -427,7 +426,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'DateFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -441,7 +440,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'RadioButtonsFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -454,13 +453,13 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'PicklistFormField': {
                 const isCountrySelector = fieldId === FieldNameToFieldId.countryCode;
                 const picklistOptions = isCountrySelector
                   ? countries.map(({ name, code }) => ({ label: name, entityId: code }))
                   : field.options;
- 
+
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
                     <Picklist
@@ -475,7 +474,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'CheckboxesFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -489,7 +488,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'PicklistOrTextFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -508,7 +507,7 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               case 'PasswordFormField': {
                 return (
                   <FieldWrapper fieldId={fieldId} key={fieldId}>
@@ -521,14 +520,14 @@ export const RegisterCustomerForm = ({
                   </FieldWrapper>
                 );
               }
- 
+
               default:
                 return null;
             }
           })}
         </div>
         <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6">
-         
+
           {reCaptchaSettings?.isEnabledOnStorefront && (
             <Field className="relative col-span-full max-w-full space-y-2 pb-7" name="ReCAPTCHA">
               <ReCaptcha
@@ -545,12 +544,12 @@ export const RegisterCustomerForm = ({
           )}
         </div>
         <div className="flex justify-center">
-        <FormSubmit asChild className="flex justify-center">
-          <SubmitButton messages={{ submit: t('submit'), submitting: t('submitting') }} />
-        </FormSubmit>
+          <FormSubmit asChild className="flex justify-center">
+            <SubmitButton messages={{ submit: t('submit'), submitting: t('submitting') }} />
+          </FormSubmit>
         </div>
       </Form>
-     
+
     </>
   );
 };
