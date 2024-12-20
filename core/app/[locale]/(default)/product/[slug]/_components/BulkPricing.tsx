44f47d81632fcd285f0009a2b371f3dfc0e6cfe0
priyanka-patra98 <priyanka@arizon.digital>
2024-12-20 14:10:23 +0530

    bulk price link

--- core/app/[locale]/(default)/product/[slug]/_components/BulkPricing.tsx
+++ core/app/[locale]/(default)/product/[slug]/_components/BulkPricing.tsx
@@ -9,7 +9,7 @@ export default function BulkPricing() {
   return (
     <div>
       <h3 className="font-semibold flex productView-info-name">Bulk Pricing  <p className="pr productView-info-value">:
-        <a href="/bulkpricing/" onClick={() => clickEventhandler}> <span className="underline hover:text-blue-500 capitalize">Click Here to Enquire</span></a>
+        <a href="/bulk-pricing" onClick={() => clickEventhandler}> <span className="underline hover:text-blue-500 capitalize">Click Here to Enquire</span></a>
       </p></h3>
     </div>
   );
