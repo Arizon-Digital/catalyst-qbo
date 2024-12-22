25993c8a9fbe209eca46343849da3ffd4a8ef7ab
priyanka-patra98 <priyanka@arizon.digital>
2024-12-22 17:45:22 +0530

    minicart link

--- core/components/ui/header/minicart.tsx
+++ core/components/ui/header/minicart.tsx
@@ -1,5 +1,6 @@
 
 
+
 'use client';
 
 import { useState, useRef, useEffect } from 'react';
@@ -23,13 +24,14 @@ export const MiniCart = ({ count }: { count: number }) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const miniBagIcons = async() => {
+    const miniBagIcons = async () => {
       let getCartIdData: any = await getCartId();
       setCartId(getCartIdData);
-      let minicartIcon: any = await  MiniCartIcon();
+      let minicartIcon: any = await MiniCartIcon();
       setMiniBag(minicartIcon);
-    }
+    };
     miniBagIcons();
+
     const handleClickOutside = (event: MouseEvent) => {
       if (cartRef.current && !cartRef.current.contains(event.target as Node)) {
         setIsOpen(false);
@@ -40,26 +42,26 @@ export const MiniCart = ({ count }: { count: number }) => {
     return () => document.removeEventListener('mousedown', handleClickOutside);
   }, []);
 
-  const loadMiniBag = async() => {
+  const loadMiniBag = async () => {
     setIsOpen(true);
     setLoading(true);
     let cartData = await getCartData();
     console.log('Full cart data:', cartData); // Debug log
-    if(cartData?.lineItems?.physicalItems) {
+    if (cartData?.lineItems?.physicalItems) {
       setCartItems(cartData);
-      if(cartData?.lineItems?.physicalItems.length > 0) {
+      if (cartData?.lineItems?.physicalItems.length > 0) {
         console.log('Physical items:', cartData.lineItems.physicalItems); // Debug log
         setHasItems(true);
       }
     }
     setLoading(false);
-  }
+  };
 
   const handleRemoveItem = async (e: React.FormEvent<HTMLFormElement>, lineItemEntityId: string) => {
     e.preventDefault();
     try {
       const result = await removeItem({
-        lineItemEntityId
+        lineItemEntityId,
       });
       loadMiniBag();
       if (result.status === 'error') {
@@ -81,12 +83,14 @@ export const MiniCart = ({ count }: { count: number }) => {
         className="relative flex items-end gap-2 p-2 rounded-full mini-cart-btn"
         aria-label="Shopping cart"
       >
-        {miniBag && <BcImage 
-          src={miniBag}
-          alt="mini-cart" 
-          width="50" 
-          height="50" 
-        />}
+        {miniBag && (
+          <BcImage
+            src={miniBag}
+            alt="mini-cart"
+            width="50"
+            height="50"
+          />
+        )}
 
         <span className="absolute right-1.5 mini-cart-count top-5 h-[20px] w-[30px] hidden sm:flex items-center justify-center rounded-full bg-[#1c2541] text-xs font-bold mini-cart-badge">
           {count}
@@ -94,7 +98,7 @@ export const MiniCart = ({ count }: { count: number }) => {
 
         <span className="text-[#1a2348] mini-cart-text hidden sm:block font-light text-sm">Cart</span>
       </button>
-      
+
       {isOpen && (
         <div className="absolute right-0 top-12 w-96 z-50 bg-white rounded-lg border border-gray-200 shadow-sm">
           <div className="p-4">
@@ -104,75 +108,76 @@ export const MiniCart = ({ count }: { count: number }) => {
                 <p className="text-red-500 text-sm">{removeError}</p>
               )}
             </div>
-          
+
             <div className="max-h-96 overflow-y-auto">
               {hasItems ? (
                 <>
                   {cartItems?.lineItems?.physicalItems?.map((item: any) => {
                     console.log('Individual item:', item); // Debug log
                     return (
-                    <div key={item.id} className="flex gap-4 py-4 border-b">
-                      <div className="w-20 h-20 bg-gray-100 rounded">
-                        {item.imageUrl ? (
-                          <img
-                            src={item.imageUrl}
-                            alt={item.name}
-                            className="w-full h-full object-contain"
-                          />
-                        ) : (
-                          <div className="w-full h-full flex items-center justify-center bg-gray-100 text-gray-400">
-                            No Image
-                          </div>
-                        )}
-                      </div>
-                      <div className="flex-1">
-                      <h3 className="text-sm text-gray-500 minicart">{item.brand}</h3>
-                        <h3 className="font-medium text-sm minicart">{item.name}</h3>
-                        
-
-                        <p className="text-sm text-gray-500 minicart">SKU: {item.sku }</p>
-                        
-                        <div className="mt-2 flex justify-between items-center">
-                          <div className="flex items-center gap-2">
-                            <span className="text-sm minicart"> {item.quantity} x</span>
-                            <div>
-                          
-                            <div className='miniprice'>
-                              <ProductPriceDisplay
-                                page="bag"
-                                currencyData={item?.extendedSalePrice?.currencyCode}
-                                product={{
-                                  prices: {
-                                    price: {
-                                      value: item.extendedSalePrice.value,
-                                      currencyCode: item.extendedSalePrice.currencyCode
-                                    }
-                                  }
-                                }}
+                      <div key={item.id} className="flex gap-4 py-4 border-b">
+                        <div className="w-20 h-20 bg-gray-100 rounded">
+                          {item.imageUrl ? (
+                            <Link href={item.url}>
+                              <img
+                                src={item.imageUrl}
+                                alt={item.name}
+                                className="w-full h-full object-contain"
                               />
+                            </Link>
+                          ) : (
+                            <div className="w-full h-full flex items-center justify-center bg-gray-100 text-gray-400">
+                              No Image
+                            </div>
+                          )}
+                        </div>
+                        <div className="flex-1">
+                          <h3 className="text-sm text-gray-500 minicart">{item.brand}</h3>
+                          <h3 className="font-medium text-sm minicart">
+                            <Link href={item.url} className="hover:underline text-blue-600">
+                              {item.name}
+                            </Link>
+                          </h3>
+                          <p className="text-sm text-gray-500 minicart">SKU: {item.sku}</p>
+                          <div className="mt-2 flex justify-between items-center">
+                            <div className="flex items-center gap-2">
+                              <span className="text-sm minicart">{item.quantity} x</span>
+                              <div className="miniprice">
+                                <ProductPriceDisplay
+                                  page="bag"
+                                  currencyData={item?.extendedSalePrice?.currencyCode}
+                                  product={{
+                                    prices: {
+                                      price: {
+                                        value: item.extendedSalePrice.value,
+                                        currencyCode: item.extendedSalePrice.currencyCode,
+                                      },
+                                    },
+                                  }}
+                                />
+                              </div>
                             </div>
                           </div>
+                          <div className="mt-2 flex justify-end">
+                            <form onSubmit={(e) => handleRemoveItem(e, item.entityId)}>
+                              <input type="hidden" name="lineItemEntityId" value={item.entityId} />
+                              <RemoveFromCartButton icon="trash" />
+                            </form>
                           </div>
-                          
-                          
-                        </div>
-                        <div className="mt-2 flex justify-end">
-                          <form onSubmit={(e) => handleRemoveItem(e, item.entityId)}>
-                            <input type="hidden" name="lineItemEntityId" value={item.entityId} />
-                            <RemoveFromCartButton icon="trash" />
-                          </form>
                         </div>
                       </div>
-                    </div>
-                  )})}
+                    );
+                  })}
                 </>
               ) : (
-              <>
-              {loading && <>Loading....</>}
-              {!loading && <div className="text-center py-8 text-gray-500">
-                  Your cart is empty
-                </div>}
-              </>
+                <>
+                  {loading && <>Loading....</>}
+                  {!loading && (
+                    <div className="text-center py-8 text-gray-500">
+                      Your cart is empty
+                    </div>
+                  )}
+                </>
               )}
             </div>
 
@@ -181,7 +186,7 @@ export const MiniCart = ({ count }: { count: number }) => {
                 <CheckoutButtonPopUp cartId={cartId} />
                 <Link
                   href="/cart"
-                  className=" w-full border !text-[13px] !p-[9px_12px] flex items-center justify-center  !mt-0 border-gray-200 text-center py-2 rounded-md hover:bg-gray-100"
+                  className="w-full border !text-[13px] !p-[9px_12px] flex items-center justify-center !mt-0 border-gray-200 text-center py-2 rounded-md hover:bg-gray-100"
                 >
                   VIEW CART
                 </Link>
